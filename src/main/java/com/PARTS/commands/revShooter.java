a8ad0dd92cff71afc9b8a374795395888e009dc9
Camtriggerhappy <ckiw23@gmail.com>
2022-05-04 09:20:08 -0400

    added shooter method

--- src/main/java/com/PARTS/commands/revShooter.java
+++ src/main/java/com/PARTS/commands/revShooter.java
@@ -18,6 +18,13 @@ public class revShooter extends CommandBase {
   /** Creates a new revShooter. */
   double velocity, maxOutput;
   beanieShooter bShooter;
+  /**
+   * 
+   * @param velocity the velocity you want to acheive
+   * @param maxOutput the maximum percent output
+   * @param bShooter a shooter implementing the beanie shooter interface.
+   * @implNote this command does not stop on its own termiate it with a command group or with some other method
+   */
   public revShooter(double velocity, double maxOutput, beanieShooter bShooter) {
     this.velocity = velocity;
     this.maxOutput = maxOutput;
@@ -41,7 +48,9 @@ public class revShooter extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    bShooter.stop();
+  }
 
   // Returns true when the command should end.
   @Override
